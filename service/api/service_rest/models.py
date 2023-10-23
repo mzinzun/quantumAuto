e00ab04cf20377f0b3d4e268cde36ae88c90c047
JohnZ <johnzak80@gmail.com>
2023-10-23 19:15:28 -0400

    initial service backend

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField()
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.CharField(max_length=200)
+    status = models.CharField(max_length=200, default="created")
+    vin = models.CharField(max_length=17, unique=False)
+    customer = models.CharField(max_length=200)
+    technician = models.ForeignKey(
+        Technician, 
+        related_name="appointments",
+        on_delete=models.CASCADE,
+        null=True
+    )
--- service/poll/poller.py
+++ service/poll/poller.py
@@ -10,17 +10,22 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service_project.settings")
 django.setup()
 
 # Import models from service_rest, here. Ignore vs-code error hinting
-# from service_rest.models import Something
-
+from service_rest.models import AutomobileVO
 
 def poll(repeat=True):
     while True:
         print('Service poller polling for data')
         try:
-            # Write your polling logic, here
-            # Do not copy entire file
-            pass
-        
+            response = requests.get('http://project-beta-inventory-api-1:8000/api/automobiles')
+            content = json.loads(response.content)
+            # print("content:", content)
+            for car in content["autos"]:
+                AutomobileVO.objects.update_or_create(
+                    defaults = {
+                        "vin": car["vin"],
+                        "sold": car["sold"]
+                    }
+                )
         except Exception as e:
             print(e, file=sys.stderr)
 
