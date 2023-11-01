25f20652ae0773dc1ef00c43d3032ae447e23207
JohnZ <johnzak80@gmail.com>
2023-11-01 10:47:38 -0400

    changed name to Quantum Auto

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,7 +1,7 @@
 function MainPage() {
   return (
     <div className="mainPage ">
-      <h1 className="text-left">CarCar</h1>
+      <h1 className="text-left">QuantumAuto</h1>
       <div className="col-lg-6 text-light">
         <p className="display-6 my-4">
           The premiere solution for automobile dealership
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
    return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-warning">
          <div className="container-fluid">
-            <NavLink className="navbar-brand" to="/">CarCar</NavLink>
+            <NavLink className="navbar-brand" to="/">QuantumAuto</NavLink>
 
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                <span className="navbar-toggler-icon"></span>
